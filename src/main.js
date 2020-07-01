5cc3ec7f4cc5f1dae3bbb2dc55c3f613fd95e307
Paula Lenis B <paulalenisb@gmail.com>
2020-07-01 16:23:15 -0500

    Intento de modales

--- src/main.js
+++ src/main.js
@@ -14,41 +14,126 @@
 
 import data from './data/pokemon/pokemon.js';
 
-const btnAboutMe = document.getElementsByClassName('btn_aboutMe'),
+/*
 overlay = document.getElementById('overlay'),
 popup= document.getElementById('popup'),
-btnClosePopup= document.getElementById('btn-close-popup');
+btnClosePopup= document.getElementById('btn-close-popup');*/
 
+let allData = data.pokemon;
 
-
-window.onload = function pokemon (allPokemon)
-{
+window.onload = function pokemon (allPokemon) {
      // function aboutMe (){ 
      //      overlay.classList.add('active');
      //      popup.classList.add('active');
      // }  
      
-    let allData = data.pokemon;
-    let card = ''
+    
+          let card = ''
          for ( let i = 0;  i < allData.length; i++) {
           
             card += `
             <li class= card>
             <h2 class="name">${allData[i].name} ${allData[i].num}</h2>
             <img class ="image_pokemon" src="${allData[i].img}" alt="pokemon-image">
-            <button class="btn_aboutMe" id="btn_aboutMe" onclick="aboutMe()" >About me</button> 
-
-            `
+            <button class="btn_aboutMe" id="btn_aboutMe">About me</button>
             
+            <div class="overlay" id="overlay" style="display:none">
+             <div class="popup" id="popup">
+             <a href="#" id="btn-close-popup" class="btn-close-popup"><i class="fas fa-times"></i> </a>
+             <img src="${allData[i].img}" alt="pokemon_card_popup">
+             <h2 class="name_popup">${allData[i].name} ${allData[i].num}</h2>
+             <div class="blue_bar"> </div>
+             <p class="type">Type: ${allData[i].type}</p>
+             <div class="container2_popup">
+             <p class="pokemon_info">Weaknesses: ${allData[i].weaknesses}</p>
+             <p class="pokemon_info">Candy: ${allData[i].candy}</p>
+             <p class="pokemon_info">Candy Count: ${allData[i].candy_count}</p>
+             <p class="pokemon_info">Spawn Chance ${allData[i].spawn_chance}</p>
+             <p class="pokemon_info">Spawn Time ${allData[i].spawn_time}</p>
+             </div>
+             </div>
+             </div>
 
-           
+            `          
           }
        document.getElementById('listOfPokemon').innerHTML = card;
-      };
+     };
 
+     const eventModal = function () {
+          let modal = document.querySelectorAll(".overlay");
+          modal.forEach(carta => {
+               let modalPopup = carta.querySelector(".popup")
+               const showInfo = () => {
+                    modalPopup.style.display = "block";
+               }
+
+               let btnAboutMe = document.querySelectorAll(".btn_aboutMe");
+               carta.addEventListener("click", showInfo());
+          })
+
+
+     }
      
 
-           function aboutMe (){ 
+
+       /*btnAboutMe.addEventListener("click", popup (allPokemon));
+
+
+       const btnAboutMe = document.getElementById('btn_aboutMe')
+
+              function popup (allPokemon) {
+
+          let popUpCard = '' 
+           for ( let i = 0;  i < allData.length; i++) {
+           
+           popUpCard += `
+      
+             
+             `
+            
+           }
+      
+        document.getElementById('container_popup').innerHTML = popUpCard;
+     
+           
+      };
+      
+      };
+
+      
+      
+
+           
+          
+      /*function popup (allPokemon) {
+      
+           let popUpCard = '' 
+           for ( let i = 0;  i < allData.length; i++) {
+           
+           popUpCard += `
+      
+             <div class="overlay" id="overlay">
+             <div class="popup" id="popup">
+             <a href="#" id="btn-close-popup" class="btn-close-popup"><i class="fas fa-times"></i> </a>
+             <img src="${allData[i].img}" alt="pokemon_card_popup">
+             <h2 class="name_popup">${allData[i].name} ${allData[i].num}</h2>
+             <div class="blue_bar"> </div>
+             <p class="type">Type: ${allData[i].type}</p>
+             <div class="container2_popup">
+             <p class="pokemon_info">Weaknesses: ${allData[i].weaknesses}</p>
+             <p class="pokemon_info">Candy: ${allData[i].candy}</p>
+             <p class="pokemon_info">Candy Count: ${allData[i].candy_count}</p>
+             <p class="pokemon_info">Spawn Chance ${allData[i].spawn_chance}</p>
+             <p class="pokemon_info">Spawn Time ${allData[i].spawn_time}</p>
+             </div>
+             `
+            
+           }
+      
+        document.getElementById('container_popup').innerHTML = popUpCard;
+       };
+
+           /*function aboutMe (){ 
                  overlay.classList.add('active');
                  popup.classList.add('active');
             }
@@ -62,7 +147,14 @@ window.onload = function pokemon (allPokemon)
 
 
 
-            });
+            });*/
+
+           
+
+     
+
+
+
 
 
             
