3575bc8a9b56015141bcff06738d304b1fd4ab52
TATIANA Toro <tatianatorog@gmail.com>
2020-07-03 11:34:29 -0500

    CAMBIOS POPUP Y FUNCION ORDENAR

--- src/main.js
+++ src/main.js
@@ -10,48 +10,106 @@ window.onload
        
      });
 
-       
+     
+
      function modal (id) {
+          let overlay = document.getElementById("overlay" + id);
           let modalPopup = document.getElementById("modal" + id);
           modalPopup.style.display = 'block';
-
+          overlay.style.display = 'block';
+          
+          console.log(modalPopup)
      };
+      
+       function closeModal (id) {
+          let overlay = document.getElementById("overlay" + id);
+          let modalPopup = document.getElementById("modal" + id);
+          modalPopup.style.display = 'none';
+          overlay.style.display = 'none';
+          
+       }
+     
+      
+     function compare( a, b ) {
+          if ( a.name< b.name ){
+            return -1;
+          }
+          if ( a.name > b.name ){
+            return 1;
+          }
+          return 0;
+        };
+
+
+     function order(name) {
+          let cardPokemon = document.getElementById("namePokemon" + name);
+          // return cardPokemon.sort( compare );
+
+          console.log(cardPokemon)   
+       }
+        
+       console.log(order())
+          
+          const selectOrderAz = document.getElementById('order_az')
+         
+              selectOrderAz.addEventListener("click", order)
+              
      
       
      function createCard (allData) {
-      
+         
           let card = ''
           for ( let i = 0;  i < allData.length; i++) {
-          
+              
             card += `
-            <li class="card" id="${allData[i].id}">
-            <h2 class="name">${allData[i].name} ${allData[i].num}</h2>
+            <li class="card" id="cards${allData[i].id}">
+            <h2 id="namePokemon${allData[i].name}" class="name">${allData[i].name}   ${allData[i].num}</h2>
             <img class ="image_pokemon" src="${allData[i].img}" alt="pokemon-image">
             <button class="btn_aboutMe" id="${allData[i].id}" onclick="modal(${allData[i].id})">About me</button>
             </li>
             
-            
+            <div class="overlay" id="overlay${allData[i].id}" style="display:none">
              <div class="popup" id="modal${allData[i].id}" style="display:none">
-             <a href="#" id="btn-close-popup" class="btn-close-popup"><i class="fas fa-times"></i> </a>
+             <a href="#" id="btn-close-popup" class="btn-close-popup" onclick="closeModal(${allData[i].id})"><i class="fas fa-times"></i> </a>
              <img src="${allData[i].img}" alt="pokemon_card_popup">
              <h2 class="name_popup">${allData[i].name} ${allData[i].num}</h2>
              <div class="blue_bar"> </div>
              <p class="type">Type: ${allData[i].type}</p>
              <div class="container2_popup">
-             <p class="pokemon_info">Weaknesses: ${allData[i].weaknesses}</p>
-             <p class="pokemon_info">Candy: ${allData[i].candy}</p>
-             <p class="pokemon_info">Candy Count: ${allData[i].candy_count}</p>
-             <p class="pokemon_info">Spawn Chance ${allData[i].spawn_chance}</p>
-             <p class="pokemon_info">Spawn Time ${allData[i].spawn_time}</p>
+             <p class="pokemon_info"> <strong> Weaknesses:</strong> ${allData[i].weaknesses}</p>
+             <p class="pokemon_info"><strong>Candy:</strong> ${allData[i].candy}</p>
+             <p class="pokemon_info"><strong>Candy Count: </strong>${allData[i].candy_count}</p>
+             <p class="pokemon_info"><strong>Spawn Chance:</strong> ${allData[i].spawn_chance}</p>
+             <p class="pokemon_info"><strong>Spawn Time:</strong> ${allData[i].spawn_time}</p>
+             </div>
              </div>
              </div>
              </div>
 
-            `      
-       
+            ` 
+            
+            
+
           };
-       
-      document.getElementById('listOfPokemon').innerHTML = card;
-    
-     };
+          let modalPopup = document.getElementById(`modal${id}`);
+            let view = card.modalPopup.map((allData) => {
+                  let candyCount = character.candy_count;
+                  if(candyCount === undefined){
+                      candyCount = 0;
+                   };
+           
+         );
      
+          document.getElementById('listOfPokemon').innerHTML = card;
+            
+          };
+          
+          
+     
+
+
+       
+      
+     
+
+   
